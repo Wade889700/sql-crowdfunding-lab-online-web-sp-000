ae338bc2872b86048c4a49c05bb84ba6beea7810
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-16 06:20:22 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,5 @@
-CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding goal INTEGER,start_date Text, end_date TEXT);
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)
+
+A user has a name and an age
+A pledge has an amount. It belongs to a user, and it also belongs to a project.
