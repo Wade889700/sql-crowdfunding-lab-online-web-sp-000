59ee0c2c93adecf38930392122ebd73ce74775d2
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-16 06:21:50 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,3 @@
 CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding goal INTEGER,start_date Text, end_date TEXT);
 CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)
-
-A user has a name and an age
-A pledge has an amount. It belongs to a user, and it also belongs to a project.
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER)
