dbfa37f6440113b976e26172e6bcb826c6a219a1
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-16 06:24:26 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,7 +1,6 @@
-CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, 
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT,
   category TEXT, funding goal INTEGER,start_date Text, end_date TEXT);
-  
+
 CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
 
 CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
-
