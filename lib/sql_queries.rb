22bc2a83bb35d8559fc037b5f1192408c294ee50
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-16 06:36:32 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,7 +18,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 " SELECT user.name, user.age, SUM(pledges.amount)
   FROM users INNER JOIN pledges
   ON user.id = pledges.user_id
-  ORDER BY user.name
+  ORDER BY users.name
  "
 end
 
